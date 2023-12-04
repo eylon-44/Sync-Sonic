628fd6ced94ecaa8c2d72040070808f83f7270f5
Eylon <eylon462@gmail.com>
2023-12-04 14:53:55 +0200

    Base commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+''' 
+Sync-Sonic Program 
+
+Repo:  	https://github.com/eylon-44/Sync-Sonic
+Author: Eylon | https://github.com/eylon-44
+'''
+
+def main():
+	return 0
+
+if (__name__ == '__main__'):
+	main()
